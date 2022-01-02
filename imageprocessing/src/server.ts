95e9a110b9514735d3aa1727013441cd2afe5d12
Ismael Bernal <ismaelbernal83@gmail.com>
2022-01-02 09:12:40 +0100

    Implementing endpoint to process images. Not working yet

--- imageprocessing/src/server.ts
+++ imageprocessing/src/server.ts
@@ -1,5 +1,7 @@
+// Node imports
 import express from 'express';
 import bodyParser from 'body-parser';
+// Own imports
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -15,9 +17,8 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
+  // endpoint to filter a   n image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -26,10 +27,15 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
   /**************************************************************************** */
-
-  //! END @TODO1
+  app.get( "/filteredimage", async ( req, res ) => {
+    // Get image_url
+    const { image_url } = req.query;
+    if (!image_url) return res.status(400).send({ message: 'Image url is mandatory'});
+    // Process image_url
+    const filteredpath = await filterImageFromURL(image_url);
+    return res.status(200).send({message: filteredpath})
+  } );
   
   // Root Endpoint
   // Displays a simple message to the user
